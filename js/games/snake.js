ca2ee93e529827795218eee711bbdf1a4b113811
Erwin Andrés <erwinlarios65@gmail.com>
2016-04-08 20:11:40 -0500

    Variables del juego -snake

--- js/games/snake.js
+++ js/games/snake.js
@@ -3,3 +3,13 @@ var canvas = document.getElementById("game");
 canvas.width = 700;
 canvas.height = 600;
 var ctx = canvas.getContext("2d");
+
+//Game variables
+var fps = 1000 / 20;
+var cell_width = 10;
+var food = {x: 0, y: 0};
+var snake = [];
+var length = 5;
+var direction = 'right';
+var direction_queue = 'right';
+var score = 0;
