593d9d9f9ea46232f9915d127a45a639a98dc7d1
Erwin Andrés <erwinlarios65@gmail.com>
2016-04-08 20:35:46 -0500

    Creando la serpiente.

--- js/games/snake.js
+++ js/games/snake.js
@@ -14,8 +14,27 @@ var direction = 'right';
 var direction_queue = 'right';
 var score = 0;
 
+//Creando nuestra protagonista
+function create_snake(){	
+	snake = [];	
+	for(i = length - 1; i >= 0; i--){		
+		snake.push({x: i, y:0});	
+	}
+}
+
 //Dibujar en el canvas
 function paint_background(){
 	ctx.fillStyle = "#333";	
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function paint_cell(x, y, fill, stroke){	
+	ctx.fillStyle = fill;	
+	ctx.fillRect(x*cell_width, y*cell_width, cell_width, cell_width);
+	
+	//Añadir un borde si fue definido
+	if(typeof stroke !== "undefined"){		
+		ctx.strokeStyle = stroke;		
+		ctx.strokeRect(x*cell_width, y*cell_width, cell_width, cell_width);	
+	}
 }
